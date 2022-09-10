c5041652b13ab0d7c248c500fb87a1e2e6c207b2
rimer1234 <83525113+rimer1234@users.noreply.github.com>
2022-09-10 16:49:52 -0400

    Add files via upload

---
+++ Card/script.js
@@ -0,0 +1,11 @@
+function opencard()
+{
+    let cover = document.getElementById('cover');
+    cover.className = 'open-card';
+}
+
+function closecard()
+{
+    let cover = document.getElementById('cover');
+    cover.className = '';
+}
